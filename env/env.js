5f1c2edb71db0fe1f84543c86912eb695bc404ac
Jatin Goyal <53302982+Jatin-Goyal5@users.noreply.github.com>
2022-07-22 19:37:02 +0530

    Delete env directory

--- env/env.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    mongoURI: "mongodb+srv://todo:tOVI7w69vLZTlrOH@cluster0.i7s9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    jwt_salt:"dksmakdanfjknnafnlajamsfklanfgabnb"
-}
