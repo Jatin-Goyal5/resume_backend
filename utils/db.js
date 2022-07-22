3006e76cac5e68c5ce712101438b5378dfda6502
Jatin Goyal <53302982+Jatin-Goyal5@users.noreply.github.com>
2022-07-22 19:38:35 +0530

    Delete utils directory

--- utils/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const env = require('../env/env');
-mongoose.connect(env.mongoURI).then(()=>{
-  console.log("Db connected");
-}).catch((e)=>{
-  console.log(e.message);
-})
-
-module.exports =mongoose;
